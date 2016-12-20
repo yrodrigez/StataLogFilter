7604e59bdb4a055be123ef4358c2dfe86c913beb
yago <yrodrigez@gmail.com>
2016-12-20 10:41:15 +0100

    First commit Created matcher to find the generated table by stata!

---
+++ StataLogFilter/src/main/java/main.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class main {
+
+    public static ArrayList<String> getParams(StringBuilder log){
+        ArrayList<String> params = new ArrayList<String>();
+
+        Pattern pattern = Pattern.compile("\\.\\s\\*\\*\\*.+\\*\\*\\*"
+                + System.getProperty("line.separator") +
+                "\\..+"
+                + System.getProperty("line.separator")
+                + System.getProperty("line.separator")
+                +".*" +
+                System.getProperty("line.separator")+
+                ".*" +
+                System.getProperty("line.separator") +
+                ".*" +
+                System.getProperty("line.separator") +
+                ".*" +
+                System.getProperty("line.separator") +
+                ".*" +
+                System.getProperty("line.separator") +
+                ".*" +
+                System.getProperty("line.separator") +
+                ".*" +
+                System.getProperty("line.separator") +
+                "-+");
+        Matcher matcher = pattern.matcher(log);
+        while (matcher.find()){
+            params.add(matcher.group());
+        }
+
+        return params;
+    }
+
+    public static StringBuilder logFileString(String path) throws FileNotFoundException {
+        File file = new File(path);
+        StringBuilder fileContents = new StringBuilder((int)file.length());
+        Scanner scanner = new Scanner(file);
+        String lineSeparator = System.getProperty("line.separator");
+
+        try {
+            while(scanner.hasNextLine()) {
+                fileContents.append(scanner.nextLine());
+                fileContents.append(lineSeparator);
+            }
+            return fileContents;
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static void main(String [] args) throws FileNotFoundException {
+        StringBuilder log = logFileString("C:\\Users\\Yago\\Downloads\\Pruebalog.log");
+
+        for(String s: getParams(log)){
+            System.out.println(s);
+        }
+
+    }
+}
